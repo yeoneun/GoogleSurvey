270e660e50fc289fd7c42d438e1b6adebacfbc0c
yeoneun <yeoneun44@gmail.com>
2023-07-05 21:27:35 +0900

    add addButton

--- src/screens/create/Create.tsx
+++ src/screens/create/Create.tsx
@@ -1,16 +1,44 @@
 import * as React from "react";
-import { SafeAreaView, ScrollView } from "react-native";
+import { Pressable, SafeAreaView, ScrollView, StyleSheet } from "react-native";
 import Wrapper from "@components/layout/Wrapper";
 import Head from "./components/Head";
+import { Ionicons } from "@expo/vector-icons";
+import GlobalStyle from "@styles/GlobalStyles";
 
 export default function HomeScreen() {
+  const addForm = () => {};
   return (
-    <ScrollView style={{ flex: 1 }} keyboardShouldPersistTaps="handled">
-      <SafeAreaView>
-        <Wrapper>
-          <Head containerStyle={{ marginTop: 10 }} />
-        </Wrapper>
-      </SafeAreaView>
-    </ScrollView>
+    <>
+      <ScrollView style={layout.scrollView} keyboardShouldPersistTaps="handled">
+        <SafeAreaView>
+          <Wrapper>
+            <Head containerStyle={layout.head} />
+          </Wrapper>
+        </SafeAreaView>
+      </ScrollView>
+      <Pressable onPress={addForm} style={addButton.container}>
+        <Ionicons name="add-sharp" size={32} color={"white"} />
+      </Pressable>
+    </>
   );
 }
+
+const layout = StyleSheet.create({
+  scrollView: { flex: 1 },
+  head: { marginTop: 10 },
+});
+
+const addButton = StyleSheet.create({
+  container: {
+    position: "absolute",
+    bottom: 30,
+    backgroundColor: GlobalStyle.point.color,
+    width: 52,
+    height: 52,
+    borderRadius: 32,
+    alignSelf: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingLeft: 2,
+  },
+});
