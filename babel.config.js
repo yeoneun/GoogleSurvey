69cd52fca7658c1d3a01878640d8c530c7c2b35a
yeoneun <yeoneun44@gmail.com>
2023-07-05 19:06:27 +0900

    react navigation, 절대경로 셋팅

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,24 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      "babel-preset-expo",
+      "module:metro-react-native-babel-preset",
+      "@babel/preset-react",
+      "@babel/preset-typescript",
+    ],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [".ios.ts", ".android.ts", ".ts", ".ios.tsx", ".android.tsx", ".tsx", ".jsx", ".js", ".json"],
+          alias: {
+            "@components": "./src/components",
+            "@screens": "./src/screens",
+          },
+        },
+      ],
+    ],
   };
 };
