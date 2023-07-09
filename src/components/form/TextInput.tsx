2dab80cb35dca246a0ba94b1d5a3d8b61ea85e63
yeoneun <yeoneun44@gmail.com>
2023-07-09 14:48:34 +0900

    edit Head

--- src/components/form/TextInput.tsx
+++ src/components/form/TextInput.tsx
@@ -1,7 +1,51 @@
 import GlobalStyle from "@styles/GlobalStyles";
-import React from "react";
-import { TextInput as RNTextInput, TextInputProps, ViewProps } from "react-native";
+import React, { useRef } from "react";
+import { TextInput as RNTextInput, TextInputProps, StyleSheet } from "react-native";
 
-export default function TextInput(props: TextInputProps) {
-  return <RNTextInput {...props} placeholderTextColor={GlobalStyle.placeholder.color} />;
+interface Props extends TextInputProps {
+  onFocus?: () => void;
+  onBlur?: () => void;
 }
+
+export default function TextInput(props: Props) {
+  const { onFocus, onBlur } = props;
+  const inputRef = useRef<RNTextInput>(null);
+
+  const handleFocus = (): void => {
+    inputRef.current?.focus();
+    inputRef.current?.setNativeProps({
+      style: [layout.textInput, layout.focusedTextInput],
+    });
+    onFocus && onFocus();
+  };
+
+  const handleBlur = (): void => {
+    inputRef.current?.blur();
+    inputRef.current?.setNativeProps({
+      style: layout.textInput,
+    });
+    onBlur && onBlur();
+  };
+
+  return (
+    <RNTextInput
+      {...props}
+      ref={inputRef}
+      placeholderTextColor={GlobalStyle.placeholder.color}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
+      style={[layout.textInput, props.style]}
+    />
+  );
+}
+
+const layout = StyleSheet.create({
+  textInput: {
+    borderBottomWidth: 1,
+    borderColor: GlobalStyle.gray.borderColor,
+  },
+  focusedTextInput: {
+    borderBottomWidth: 2,
+    borderColor: GlobalStyle.point.color,
+  },
+});
--- src/screens/create/components/Head.tsx
+++ src/screens/create/components/Head.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useRef, useState } from "react";
-import { StyleSheet, TextInput, Animated, ViewStyle } from "react-native";
+import { StyleSheet, Animated, ViewStyle } from "react-native";
 import GlobalStyle from "styles/GlobalStyles";
 import TextDecoration from "@components/form/TextDecoration";
 import Container from "./Container";
 import { useAppDispatch, useAppSelector } from "hooks/useRedux";
 import { setDescription, setTitle } from "utils/redux/slices/formSlice";
+import TextInput from "@components/form/TextInput";
 
 export default function Head() {
   const form = useAppSelector((state) => state.form);
@@ -13,7 +14,6 @@ export default function Head() {
   const dispatchTitle = (value: string) => dispatch(setTitle(value));
   const dispatchDescription = (value: string) => dispatch(setDescription(value));
 
-  const [titleFocus, setTitleFocus] = useState(false);
   const [descriptionFocus, setDescriptionFocus] = useState(false);
 
   const titleAreaMarginTop = useRef(new Animated.Value(0)).current;
@@ -21,95 +21,92 @@ export default function Head() {
   const descriptionAreaMarginTop = useRef(new Animated.Value(10)).current;
   const descriptionAreaDecorationHeight = useRef(new Animated.Value(0)).current;
 
-  useEffect(() => {
-    if (titleFocus) {
-      Animated.parallel([
-        Animated.timing(titleAreaMarginTop, {
-          toValue: 24,
-          duration: 300,
-          useNativeDriver: false,
-        }),
-        Animated.timing(titleAreaDecorationHeight, {
-          toValue: 37,
-          duration: 300,
-          useNativeDriver: false,
-        }),
-      ]).start();
-    } else {
-      Animated.parallel([
-        Animated.timing(titleAreaMarginTop, {
-          toValue: 0,
-          duration: 300,
-          useNativeDriver: false,
-        }),
-        Animated.timing(titleAreaDecorationHeight, {
-          toValue: 0,
-          duration: 300,
-          useNativeDriver: false,
-        }),
-      ]).start();
-    }
-  }, [titleFocus]);
-
-  useEffect(() => {
-    if (descriptionFocus) {
-      Animated.parallel([
-        Animated.timing(descriptionAreaMarginTop, {
-          toValue: 24,
-          duration: 300,
-          useNativeDriver: false,
-        }),
-        Animated.timing(descriptionAreaDecorationHeight, {
-          toValue: 37,
-          duration: 300,
-          useNativeDriver: false,
-        }),
-      ]).start();
-    } else {
-      Animated.parallel([
-        Animated.timing(descriptionAreaMarginTop, {
-          toValue: 10,
-          duration: 300,
-          useNativeDriver: false,
-        }),
-        Animated.timing(descriptionAreaDecorationHeight, {
-          toValue: 0,
-          duration: 300,
-          useNativeDriver: false,
-        }),
-      ]).start();
-    }
-  }, [descriptionFocus]);
+  const onTitleFocus = () => {
+    Animated.parallel([
+      Animated.timing(titleAreaMarginTop, {
+        toValue: 24,
+        duration: 300,
+        useNativeDriver: false,
+      }),
+      Animated.timing(titleAreaDecorationHeight, {
+        toValue: 37,
+        duration: 300,
+        useNativeDriver: false,
+      }),
+    ]).start();
+  };
+  const onTitleBlur = () => {
+    Animated.parallel([
+      Animated.timing(titleAreaMarginTop, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: false,
+      }),
+      Animated.timing(titleAreaDecorationHeight, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: false,
+      }),
+    ]).start();
+  };
+  const onDescriptonFocus = () => {
+    Animated.parallel([
+      Animated.timing(descriptionAreaMarginTop, {
+        toValue: 24,
+        duration: 300,
+        useNativeDriver: false,
+      }),
+      Animated.timing(descriptionAreaDecorationHeight, {
+        toValue: 37,
+        duration: 300,
+        useNativeDriver: false,
+      }),
+    ]).start();
+  };
+  const onDescriptonBlur = () => {
+    Animated.parallel([
+      Animated.timing(descriptionAreaMarginTop, {
+        toValue: 10,
+        duration: 300,
+        useNativeDriver: false,
+      }),
+      Animated.timing(descriptionAreaDecorationHeight, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: false,
+      }),
+    ]).start();
+  };
 
   return (
     <Container style={layout.container}>
-      <Animated.View style={[titleArea.container, { marginTop: titleAreaMarginTop }]}>
+      <Animated.View style={{ marginTop: titleAreaMarginTop }}>
         <TextInput
           value={form.title}
           onChangeText={dispatchTitle}
           placeholder="설문지 제목"
           placeholderTextColor={GlobalStyle.placeholder.color}
-          onFocus={() => setTitleFocus(true)}
-          onBlur={() => setTitleFocus(false)}
-          style={[titleArea.textInput, titleFocus && titleArea.textInputFocus]}
+          style={layout.titleInput}
+          onFocus={onTitleFocus}
+          onBlur={onTitleBlur}
           multiline
         />
-        <Animated.View style={[{ overflow: "hidden" }, { height: titleAreaDecorationHeight }]}>
+        <Animated.View style={[layout.textDecorationArea, { height: titleAreaDecorationHeight }]}>
           <TextDecoration />
         </Animated.View>
       </Animated.View>
-      <Animated.View style={[descriptionArea.container, { marginTop: descriptionAreaMarginTop }]}>
+      <Animated.View style={{ marginTop: descriptionAreaMarginTop }}>
         <TextInput
           value={form.description}
           onChangeText={dispatchDescription}
           placeholder="설문지 설명"
           placeholderTextColor={GlobalStyle.placeholder.color}
-          onFocus={() => setDescriptionFocus(true)}
-          onBlur={() => setDescriptionFocus(false)}
-          style={[descriptionArea.textInput, descriptionFocus && descriptionArea.textInputFocus]}
+          onFocus={onDescriptonFocus}
+          onBlur={onDescriptonBlur}
+          style={layout.descriptionInput}
           multiline
         />
-        <Animated.View style={[{ overflow: "hidden" }, { height: descriptionAreaDecorationHeight }]}>
+        <Animated.View style={[layout.textDecorationArea, { height: descriptionAreaDecorationHeight }]}>
           <TextDecoration />
         </Animated.View>
       </Animated.View>
@@ -117,44 +114,22 @@ export default function Head() {
   );
 }
 
-const sharedStyles = {
-  input: {
-    borderBottomWidth: 1,
-    borderColor: GlobalStyle.gray.borderColor,
-  },
-  inputFocus: {
-    borderBottomWidth: 2,
-    borderColor: GlobalStyle.point.color,
-  },
-};
-
 const layout = StyleSheet.create({
   container: {
     borderTopColor: GlobalStyle.point.color,
     borderTopWidth: 8,
     marginTop: 10,
   },
-});
-
-const titleArea = StyleSheet.create({
-  container: {},
-  textInput: {
-    ...sharedStyles.input,
+  textDecorationArea: { overflow: "hidden" },
+  titleInput: {
     fontSize: 28,
     fontWeight: "500",
     paddingVertical: 28 * 0.2,
   },
-  textInputFocus: sharedStyles.inputFocus,
-});
-
-const descriptionArea = StyleSheet.create({
-  container: { marginTop: 10 },
-  textInput: {
-    ...sharedStyles.input,
+  descriptionInput: {
     fontSize: 15,
     paddingVertical: 15 * 0.2,
     borderBottomWidth: 1,
     borderColor: GlobalStyle.gray.borderColor,
   },
-  textInputFocus: sharedStyles.inputFocus,
 });
