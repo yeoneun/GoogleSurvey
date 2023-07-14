c3c1299e990fb1e1fe9109eb2ed8df6403473745
yeoneun <yeoneun44@gmail.com>
2023-07-14 17:38:29 +0900

    fix android statusBar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,7 @@
 import * as React from "react";
+import Constants from "expo-constants";
+import { Platform, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { DefaultTheme } from "@react-navigation/native";
@@ -14,25 +17,31 @@ const Stack = createNativeStackNavigator<ScreenParams>();
 
 export default function App() {
   return (
-    <ActionSheetProvider>
-      <Provider store={store}>
-        <GestureHandlerRootView style={{ flex: 1 }}>
-          <NavigationContainer
-            theme={{
-              ...DefaultTheme,
-              colors: {
-                ...DefaultTheme.colors,
-                background: "#F0ECF8",
-              },
-            }}
-          >
-            <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Create">
-              <Stack.Screen name="Create" component={Create} />
-              <Stack.Screen name="Preview" component={Preview} />
-            </Stack.Navigator>
-          </NavigationContainer>
-        </GestureHandlerRootView>
-      </Provider>
-    </ActionSheetProvider>
+    <>
+      <View style={{ paddingTop: Platform.OS === "ios" ? 0 : Constants.statusBarHeight }}>
+        <StatusBar />
+      </View>
+
+      <ActionSheetProvider>
+        <Provider store={store}>
+          <GestureHandlerRootView style={{ flex: 1 }}>
+            <NavigationContainer
+              theme={{
+                ...DefaultTheme,
+                colors: {
+                  ...DefaultTheme.colors,
+                  background: "#F0ECF8",
+                },
+              }}
+            >
+              <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Create">
+                <Stack.Screen name="Create" component={Create} />
+                <Stack.Screen name="Preview" component={Preview} />
+              </Stack.Navigator>
+            </NavigationContainer>
+          </GestureHandlerRootView>
+        </Provider>
+      </ActionSheetProvider>
+    </>
   );
 }
